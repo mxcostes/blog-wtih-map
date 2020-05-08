5c1b895bded0bcf95a186cc0d7af4987958f2069
Max-Costes <mxcostes@gmail.com>
2020-05-08 14:28:36 -0400

    button margin on homepage

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -92,7 +92,7 @@ export class HomeBody extends Component {
 			<div>
 				<span>
 					<h1>Explore</h1>
-					<Button className='pb-3' onClick={this.toggleMapLayer}>
+					<Button className='mb-3' onClick={this.toggleMapLayer}>
 						{this.state.mapLayer ? 'See Lines' : 'See Sat Imagery'}
 					</Button>
 				</span>
